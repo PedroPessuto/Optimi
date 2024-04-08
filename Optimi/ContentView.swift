0b9b0b7e9c4b39c1823c5ad383c3916805c0f700
Pedro Pessuto <pedro.pessuto@outlook.com>
2024-04-08 13:55:55 -0300

    Initial Commit

---
+++ Optimi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Optimi
+//
+//  Created by Pedro Pessuto on 08/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Optimi/OptimiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptimiApp.swift
+//  Optimi
+//
+//  Created by Pedro Pessuto on 08/04/24.
+//
+
+import SwiftUI
+
+@main
+struct OptimiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
